831035a3898b8fd34fe0406ced1f8bba98875b40
Mobin Javari <mobinjavari@duck.com>
2023-10-31 02:03:13 +0330

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		while (true) {
+			float fr;
+			float pi = 3.14f;
+			Scanner input;
+			
+			input = new Scanner(System.in);
+			
+			System.out.println("Plz inter ft:");
+			fr = input.nextFloat();
+			
+		    System.out.println(4 * pi * (fr * fr));
+		}
+	  }
+}
